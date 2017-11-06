c3ef2228e721a26c42b6e33e9e0ac97c38d5ad2d
rasiyanir <33336512+rasiyanir@users.noreply.github.com>
2017-11-05 19:18:49 -0600

    Update home.component.js

--- Angular2MVC/app/Components/home.component.js
+++ Angular2MVC/app/Components/home.component.js
@@ -1,20 +1,78 @@
-"use strict";
-var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
-    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
-    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
-    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
-    return c > 3 && r && Object.defineProperty(target, key, r), r;
-};
-var core_1 = require("@angular/core");
-var HomeComponent = (function () {
-    function HomeComponent() {
+import { Component, OnInit } from "@angular/core";
+import { UserService } from '../Service/user.service';
+import { Global } from '../Shared/global';
+import { IUser } from '../Model/user';
+
+@Component({
+    template: `<img src="../../images/users.png" style="text-align:center"/>
+<br/>
+<div name="userNameDiv">
+    <label for="username">Username</label>
+    <input #username type="text" name="username" required />
+</div>
+
+<div name="passwordDiv">
+    <label for="password">Password</label>
+    <input #password type="password" name="password" required />
+</div>
+<div name="buttonDiv">
+    <button (click)="login(username.value, password.value)" class="btn btn-primary">Login</button>
+</div>
+<br/>
+    {{clickMessage}}
+`
+
+})
+
+export class HomeComponent implements OnInit {
+
+    clickMessage = '';
+    users: IUser[];
+    user: IUser;
+    msg: string;
+    indLoading: boolean = false;
+
+    constructor(private _userService: UserService) { }
+    ngOnInit(): void {
+        this.LoadUsers();
     }
-    return HomeComponent;
-}());
-HomeComponent = __decorate([
-    core_1.Component({
-        template: "<img src=\"../../images/users.png\" style=\"text-align:center\"/>"
-    })
-], HomeComponent);
-exports.HomeComponent = HomeComponent;
-//# sourceMappingURL=home.component.js.map
+
+
+    login(username: string, password: string) {
+
+        var usernameIsFound = false;
+        var passwordDoesNotMatch = false;
+
+        for (var i = 0; i < this.users.length; i++) {
+            if (this.users[i].LastName == username) {
+                usernameIsFound = true;
+                if (password == this.users[i].FirstName) {
+                    this.clickMessage = 'Login Successfully!';
+                    return;
+                }
+                else {
+                    passwordDoesNotMatch = true;
+                }
+            }
+        }
+
+
+
+        this.clickMessage = 'Login Failed!';
+        if (usernameIsFound)
+            this.clickMessage = this.clickMessage + " Due to not matching password!";
+        else
+            this.clickMessage = this.clickMessage + " User is not found!";
+
+    }
+
+
+
+    LoadUsers(): void {
+        this.indLoading = true;
+        this._userService.get(Global.BASE_USER_ENDPOINT)
+            .subscribe(users => { this.users = users; this.indLoading = false; },
+            //error => this.msg = <any>error
+        );
+    }
+}
